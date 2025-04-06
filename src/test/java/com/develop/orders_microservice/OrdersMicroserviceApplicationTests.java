e0708011b096ea937a5e561c03052a2bf0cffd8f
Juan Hoyos <juanhoyos1347@gmail.com>
2025-04-06 02:34:58 -0500

    Feat: structure

---
+++ src/test/java/com/develop/orders_microservice/OrdersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.orders_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
